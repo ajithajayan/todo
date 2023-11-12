04269480786e523a834783c1c5b2cab06f251b00
Ajith Ajayan <ajithajayan222aa@gmail.com>
2023-11-12 19:34:23 +0530

    to do list insertion and deletion done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  let [toDos,setToDos]=useState([])
+  let [toDo,setToDo]=useState("")
+  
+const currentDate = new Date();
+const dayOfWeek = currentDate.getDay();
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const currentDay = daysOfWeek[dayOfWeek];
+
+
+
+
+  const deleteTodo = (index) => {
+    const updatedTodos = toDos.filter((_, i) => i !== index);
+    setToDos(updatedTodos);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's {currentDay} 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>{setToDos([...toDos,toDo]);setToDo('');}} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {
+          toDos.map((value,index)=>{
+            return(
+                <div className="todo" key={index}>
+                  <div className="left">
+                    <input type="checkbox" name="" id="" />
+                    <p >{value}</p>
+                  </div>
+                  <div className="right">
+                    <i onClick={()=>deleteTodo(index)} className="fas fa-times"></i>
+                  </div>
+                </div>)
+        })}
+      </div>
     </div>
   );
 }
